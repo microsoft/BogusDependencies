8eebbd7a9e2d0645cc713c8c906024752908c79e
Erik Mavrinac <erikmav@microsoft.com>
2020-07-23 14:30:41 -0700

    Create started library and test projects along with initial nupkg build

---
+++ src/Lib/BogusDependencyDetectionFactory.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace Microsoft.Build.BogusDependencies
+{
+    /// <summary>
+    /// Provides a public factory for Bogus Dependency Detection implementations.
+    /// </summary>
+    public class BogusDependencyDetectionFactory
+    {
+    }
+}
---
+++ src/Test/BogusDependencyDetectionFactoryTests.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BogusDependenciesTests
+{
+    [TestClass]
+    public class BogusDependencyDetectionFactoryTests
+    {
+    }
+}
